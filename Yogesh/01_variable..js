8019589a7791734b9f169db2ccf07376f1e842b5
Yogesh Kumar <130029916+yogeshkumar13@users.noreply.github.com>
2023-10-09 15:04:25 +0000

    main

---
+++ Yogesh/01_variable..js
@@ -0,0 +1,16 @@
+const accountId=145221
+let accountEmail="dokdk@gmail.com"
+ var acoountPassword="125621"
+acoountCity="bareilly"
+
+//acoountId=2
+accountEmail="def@.com"
+accountPassword="54811"
+acoountCity="Jaipur"
+//accountId="565656"
+let acoountState;
+console.log([acoountPassword,accountEmail,acoountCity])
+
+
+
+
