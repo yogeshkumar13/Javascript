8220f001dad901cd1f3ba3eb65ee30ad6a8042c2
Yogesh Kumar <130029916+yogeshkumar13@users.noreply.github.com>
2023-10-10 18:47:17 +0000

    main

---
+++ datatype-summary.js
@@ -0,0 +1,19 @@
+//primtive- 
+
+// 7 types : String,Number,boolean,Null,BigInt,Undefined,symbol.
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid)
+
+  const bigNumber = 56555415485154841n
+
+//Referance (Non primitive)-
+
+//Array,Objects,Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj={
+    name:"Yogesh",
+    age:22,
+
+}
