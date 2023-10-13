ccf75de3f3c006826f7ec07730e8a8eb3db77044
Yogesh Kumar <130029916+yogeshkumar13@users.noreply.github.com>
2023-10-13 17:01:26 +0000

    main

--- 02_conversionOperation.js
+++ 02_conversionOperation.js
@@ -3,7 +3,7 @@ let score= null
 //console.log(typeof score);
 //console.log(typeof (score));
 
-//let valueInNumber = Number(score)
+//let valueInNumber = Number(score)  
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber)
 
