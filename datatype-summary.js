2cd3931dac3a1058c7feec2377e73476c544de0a
Yogesh Kumar <130029916+yogeshkumar13@users.noreply.github.com>
2023-10-14 17:22:47 +0000

    main

--- datatype-summary.js
+++ datatype-summary.js
@@ -16,6 +16,32 @@ let myobj={ // Declear objects
     name:"Yogesh",
     age:22,
 }
- const myfunction =function({ // Declear Function
-  console.log("Hello world")
- })
+ //const myfunction =function({ // Declear Function
+ // console.log("Hello world")
+ //})
+
+
+
+ // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ // Stack,Heap Memory -
+ // Stack(primitive dt) -primitive datatype mein stack memory use hote hai.
+ // Heap(non-primitive dt) - non-primitive datatype mein heap memory use hote hai.
+
+ let myYoutube ="yogeshgangwar"
+ let anothername = myYoutube
+  anothername ="dhbehje"
+ console.log(myYoutube);
+ console.log(anothername);
+
+ let userOne={
+   email :"yogesh@gmail.com",
+   upi:"yogesh@ybl"
+ }
+
+ let userTwo =userOne
+
+ usertwo.email ="djfndj@gmail.com"
+  console.log(userOne.email);
+  console.log(userTwo.email);
+ 
